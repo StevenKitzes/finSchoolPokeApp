049626f8c9ba2c9dc3174096d4824da1af89ee70
Steven Kitzes <steven.kitzes@parivedasolutions.com>
2016-08-18 19:10:26 -0500

    Implemented random selection of Pokemon by city (by Pokemon type)

--- offices.js
+++ offices.js
@@ -1,1 +1,29 @@
-offices.js
+var officeData = require('./officeData');
+var fullList = require('./pokemonList');
+var _ = require('lodash');
+
+var getPokemonByOffice = function(office) {
+  console.log('getting pokemon by office: ' + office);
+  var officeTypes = officeData[office]; //hopefully this is an array
+  var subList = [];
+
+  // for each pokemon
+  for(var p = 0; p < fullList.length; p++) {
+    var current = fullList[p];
+
+    // for each type this pokemon belongs to
+    for(var t = 0; t < current['types'].length; t++) {
+      if(officeTypes.indexOf(current['types'][t]) != -1) {
+        subList.push(current);
+        break;
+      }
+    }
+  }
+
+  // sort subList using lodash
+  subList = _.sortBy(subList, 'height');
+
+  return subList; // return list of the pokemon represented at the queried office
+}
+
+module.exports = getPokemonByOffice;
